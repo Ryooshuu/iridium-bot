4f6bcc9a9f6d621c866cb01b4a771ea67a0a707a
Irisu <nekomarisakirisame@gmail.com>
2023-03-29 05:47:09 -0400

    add our own command store

--- src/structures/IridiumCommand.ts
+++ src/structures/IridiumCommand.ts
@@ -1,8 +1,11 @@
 import { Args, Command, CommandOptions, PieceContext } from "@sapphire/framework";
 
 export class IridiumCommand<PreParseReturn = Args, O extends IridiumCommandOptions = IridiumCommandOptions> extends Command<PreParseReturn, O> {
+    public module: string | null;
     constructor(context: PieceContext, options: O = {} as O) {
         super(context, options);
+
+        this.module = options.module ?? null;
     }
 } 
 
