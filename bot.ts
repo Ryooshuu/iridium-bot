554fbc0caac8ff8185a84148387c64ebabc4241d
Irisu <nekomarisakirisame@gmail.com>
2023-03-29 01:03:47 -0400

    fix intents

--- bot.ts
+++ bot.ts
@@ -5,7 +5,15 @@ import { Iridium } from "./src/Iridium";
 import { GatewayIntentBits  } from "discord.js";
 
 new Iridium({
-    intents: [GatewayIntentBits.MessageContent],
+    intents: [
+        GatewayIntentBits.DirectMessageReactions,
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessageReactions,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.MessageContent,
+    ],
     loadMessageCommandListeners: true,
     defaultPrefix: process.env.IRIDIUM_PREFIX,
 });
